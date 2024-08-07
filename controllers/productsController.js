6c3ed819172f35bb42e57058938fef3ba01b665d
Natanael <natancnascimento88@gmail.com>
2024-08-07 10:35:40 -0300

    modificado apiArquivo

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -28,7 +28,68 @@ const addProduct = (req, res) => {
     );
 };
 
+const updateProductPut = (req, res) => {
+    const {id} = req.params;
+    const {name, description, category, price, stock, expiry_date} = req.body;
+    db.query(
+        'UPDATE products SET name=?, description=?, category=?, price=?, stock=?, expiry_date=? WHERE id=?',
+        [name, description, category, price, stock, expiry_date, id],
+        (err, results) => {
+            if(err) {
+                console.error('erro ao adicionar produto', err);
+                res.status(500).send('erro ao adiconar produto');
+                return;
+            }
+            res.send('produto atualizado com sucesso')
+        }
+    );
+};
+
+const updateProductPatch = (req, res) => {
+    const{id} = req.params;
+    const fields = req.body;
+    const query = [];
+    const values = [];
+
+    for(const[key,value] of Object.entries(fields)) {
+        query.push (`${key} = ?`);
+        values.push(value);
+    }
+    values.push(id);
+
+    db.query(
+        `UPDATE products set ${query.join(',')} WHERE id = ?`,
+        values,
+        (err, results) => {
+            if(err) {
+                console.error('erro ao adicionar produto', err);
+                res.status(500).send('erro ao adicionar produto');
+                return;
+            }
+            res.send('produtos atualizado com sucesso');
+        }
+        
+    );
+};
+
+const deleteProduct = (req,res) => {
+    const{id} = req.params;
+    db.query('DELETE FROM Products WHERE id = ?',[id],
+        (err, results) => {
+            if(err) {
+                console.error('erro ao adicionar produto', err);
+                res.status(500).send('erro ao deletar produto');
+                return;
+            }
+            res.send('produto deletado com sucesso');
+        }
+    );
+};
+
 module.exports = {
     getAllProducts,
-    addProduct
+    addProduct,
+    updateProductPut,
+    updateProductPatch,
+    deleteProduct
 };
